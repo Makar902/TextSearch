557373b2e86970a2ca827b1c2502505131b96fd9
Makar <byreymakar@gmial.com>
2023-09-04 22:44:13 +0300

    Add project files.

---
+++ Ex/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Ex
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBox1 = new ListBox();
+            button2 = new Button();
+            button3 = new Button();
+            textBox1 = new TextBox();
+            button4 = new Button();
+            progressBar1 = new ProgressBar();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(559, 45);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(233, 289);
+            listBox1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(717, 415);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 1;
+            button2.Text = "Exit";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(12, 32);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 2;
+            button3.Text = "Start";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(93, 32);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(207, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // button4
+            // 
+            button4.BackgroundImageLayout = ImageLayout.None;
+            button4.Location = new Point(559, 340);
+            button4.Name = "button4";
+            button4.Size = new Size(233, 23);
+            button4.TabIndex = 4;
+            button4.Text = "Browse your text...";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(12, 61);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(288, 23);
+            progressBar1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(804, 450);
+            Controls.Add(progressBar1);
+            Controls.Add(button4);
+            Controls.Add(textBox1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(listBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private ListBox listBox1;
+        private Button button2;
+        private Button button3;
+        private TextBox textBox1;
+        private Button button4;
+        private ProgressBar progressBar1;
+    }
+}
---
+++ Ex/Form1.cs
@@ -0,0 +1,384 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Security.Principal;
+
+namespace Ex
+{
+    public partial class Form1 : Form
+    {
+
+        private volatile bool cancelSearch = false;
+        private object searchLock = new object();
+
+        private ComboBox comboBoxWords;
+        private Label resultsLabel;
+        private Button pauseButton;
+        private Button stopButton;
+        private Button exportButton;
+        private Button openFolderButton;
+        private Button saveReportButton;
+        private string TextBoxMessege;
+        private const string LogTXT = "Log.txt";
+
+
+
+
+
+        public Form1()
+        {
+            WindowsIdentity currentIdentity = WindowsIdentity.GetCurrent();
+            WindowsPrincipal currentPrincipal = new WindowsPrincipal(currentIdentity);
+
+            if (currentPrincipal.IsInRole(WindowsBuiltInRole.Administrator))
+            {
+                MessageBox.Show("Програма запущена з правами адміністратора.");
+            }
+            else
+            {
+                MessageBox.Show("Програма не запущена з правами адміністратора.");
+
+
+            }
+
+
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+
+            TextBoxInit();
+            OnForm();
+            MenuStripInit();
+        }
+        /*-----------------------------------------------------------------*/
+        //TextBox1 Init
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void TextBoxInit()
+        {
+            try
+            {
+                TextBoxMessege = "Enter your text here...";
+                textBox1.Text = TextBoxMessege;
+                textBox1.Enter += TextBox1_Enter1;
+                textBox1.Leave += TextBox1_Leave;
+                TextBox1_Leave(textBox1, null);
+                TextBox1_Leave(textBox1, null);
+            }
+            catch (Exception error)
+            {
+
+                CatchExToLog(error);
+            }
+        }
+        /*-----------------------------------------------------------------*/
+
+
+        private void importMenuItem_Click(object sender, EventArgs e)
+        {
+            // Обробник події для "Імпорт"
+            MessageBox.Show("Ви натиснули на 'Імпорт'");
+        }
+
+        private void exportMenuItem_Click(object sender, EventArgs e)
+        {
+            // Обробник події для "Експорт"
+            MessageBox.Show("Ви натиснули на 'Експорт'");
+        }
+
+        private void exitMenuItem_Click(object sender, EventArgs e)
+        {
+            // Обробник події для "Вихід"
+            Application.Exit();
+        }
+
+        private void aboutMenuItem_Click(object sender, EventArgs e)
+        {
+            // Обробник події для "Про програму"
+            MessageBox.Show("Про програму:\nВерсія 1.0\nАвтор: Ваше ім'я");
+        }
+        public void OnForm()
+        {
+            
+
+            comboBoxWords = new ComboBox();
+            comboBoxWords.Location = new Point(10, 120);
+            comboBoxWords.Size = new Size(200, 30);
+            this.Controls.Add(comboBoxWords);
+
+
+
+            resultsLabel = new Label();
+            resultsLabel.Location = new Point(10, 160);
+            resultsLabel.Size = new Size(300, 100);
+            resultsLabel.Text = "Results will be displayed here.";
+            this.Controls.Add(resultsLabel);
+
+
+        }
+        /*-----------------------------------------------------------------*/
+        //MenuStrip
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void MenuStripInit()
+        {
+            try
+            {
+                MenuStrip menuStrip = new MenuStrip();
+
+                ToolStripMenuItem fileMenuItem = new ToolStripMenuItem("File");
+                ToolStripMenuItem editMenuItem = new ToolStripMenuItem("View");
+                ToolStripMenuItem helpMenuItem = new ToolStripMenuItem("About");
+
+                menuStrip.Items.Add(fileMenuItem);
+                menuStrip.Items.Add(editMenuItem);
+                menuStrip.Items.Add(helpMenuItem);
+
+                this.MainMenuStrip = menuStrip;
+
+                this.Controls.Add(menuStrip);
+            }
+            catch (Exception error)
+            {
+
+                CatchExToLog(error);            }
+        }
+        /*-----------------------------------------------------------------*/
+
+
+        /*-----------------------------------------------------------------*/
+        //Search&CopyFiles
+        private void SearchFilesAndDirectories(string directoryWhere, string wordsToSearch)
+        {
+            directoryWhere = "E:\\TEST";
+            try
+            {
+                string[] files = Directory.GetFiles(directoryWhere);
+                int totalFiles = files.Length;
+                int processedFiles = 0;
+
+                foreach (string file in files)
+                {
+                    listBox1.Items.Add(file);
+                    if (cancelSearch)
+                    {
+                        return;
+                    }
+
+                    string fileContents = File.ReadAllText(file);
+                    if (fileContents.Contains(wordsToSearch))
+                    {
+                        string path_to = Directory.GetCurrentDirectory() + '\\';
+                        CopyAndModifyFile(directoryWhere, path_to, wordsToSearch);
+                        MessageBox.Show("Dire bef in Search^ " + directoryWhere);
+                    }
+
+                    processedFiles++;
+                    UpdateProgressBar(processedFiles, totalFiles);
+                }
+
+                string[] subDirectories = Directory.GetDirectories(directoryWhere);
+                foreach (string subDirectory in subDirectories)
+                {
+                    SearchFilesAndDirectories(subDirectory, wordsToSearch);
+                }
+            }
+            catch (Exception ex)
+            {
+                CatchExToLog(ex);
+            }
+        }
+        private void CopyAndModifyFile(string directoryWhere, string path_to, string wordsToSearch)
+        {
+            try
+            {
+
+                MessageBox.Show("dir in copy show: " + directoryWhere);
+
+                string fileName = Path.GetFileName(directoryWhere);
+                // string patht = "E:\\TEST\\123.txt";
+                string ResPath = Path.Combine(path_to, fileName);
+                File.Copy(directoryWhere, ResPath, true);
+
+                string fileContents = File.ReadAllText(ResPath);
+
+                fileContents = fileContents.Replace(wordsToSearch, "*******");
+
+                File.WriteAllText(ResPath, fileContents);
+
+            }
+            catch (Exception ex)
+            {
+                CatchExToLog(ex);
+            }
+        }
+        /*-----------------------------------------------------------------*/
+
+
+        /*-----------------------------------------------------------------*/
+        //ProgeressBar
+        private void UpdateProgressBar(int processedFiles, int totalFiles)
+        {
+            try
+            {
+                if (InvokeRequired)
+                {
+                    BeginInvoke(new Action<int, int>(UpdateProgressBar), processedFiles, totalFiles);
+                    return;
+                }
+
+                progressBar1.Value = (int)(((double)processedFiles / totalFiles) * 100);
+                if (progressBar1.Value == progressBar1.Maximum)
+                {
+                    MessageBox.Show("Finished!");
+                    progressBar1.Value = 0;
+                }
+            }
+            catch (Exception error)
+            {
+
+                CatchExToLog(error);
+            }
+        }
+        /*-----------------------------------------------------------------*/
+
+
+        /*-----------------------------------------------------------------*/
+        //Exception procesing
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void CatchExToLog(Exception error)
+        {
+            try
+            {
+    
+                using (StreamWriter log = new StreamWriter(LogTXT, true))
+                {
+                    log.WriteLine(error.ToString());
+                }
+            }
+            catch (Exception serror)
+            {
+                using (StreamWriter log = new StreamWriter(LogTXT, true))
+                {
+                    log.WriteLine(serror.ToString());
+                }
+            }
+        }
+        /*-----------------------------------------------------------------*/
+
+        /*-----------------------------------------------------------------*/
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            cancelSearch = true;
+        }
+        private void exportButton_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void importButton_Click(object sender, EventArgs e)
+        {
+
+        }
+        /*-----------------------------------------------------------------*/
+
+        /*-----------------------------------------------------------------*/
+        //exit button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult result = MessageBox.Show("Exit program?", "Exit", MessageBoxButtons.YesNo);
+
+                if (result == DialogResult.Yes)
+                {
+                    Application.Exit();
+                }
+                else if (result == DialogResult.No)
+                {
+                }
+            }
+            catch (Exception error)
+            {
+
+                CatchExToLog(error);
+            }
+        }
+        //start button
+        private void button3_Click(object sender, EventArgs e)
+        {
+            cancelSearch = false;
+            string wSearch = textBox1.Text;
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+            Thread searchThread = new Thread(() =>
+            {
+
+                foreach (DriveInfo drive in allDrives)
+                {
+                    if (drive.IsReady)
+                    {
+                        string rootDirectory = drive.RootDirectory.FullName;
+                        SearchFilesAndDirectories(rootDirectory, wSearch);
+                        //MessageBox.Show(rootDirectory);
+                    }
+                }
+            });
+
+            searchThread.Start();
+        }
+        //Browse button
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.Multiselect = true;
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string[] UserFilePath = openFileDialog.FileNames;
+                    for (int i = 0; i < UserFilePath.Length; i++)
+                    {
+                        listBox1.Items.Add(UserFilePath[i]);
+                    }
+                }
+            }
+            catch (Exception error)
+            {
+
+                CatchExToLog(error);
+            }
+        }
+        /*-----------------------------------------------------------------*/
+
+        /*-----------------------------------------------------------------*/
+        //TextBox
+        private void TextBox1_Leave(object? sender, EventArgs? e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(textBox1.Text))
+                {
+                    textBox1.Text = TextBoxMessege;
+                }
+            }
+            catch (Exception error)
+            {
+
+                CatchExToLog(error);
+            }
+        }
+        //TextBox
+        private void TextBox1_Enter1(object? sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox1.Text == TextBoxMessege)
+                {
+                    textBox1.Text = "";
+                }
+            }
+            catch (Exception error)
+            {
+
+                CatchExToLog(error);            }
+        }
+        /*-----------------------------------------------------------------*/
+
+    }
+}
